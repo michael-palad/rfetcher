36f0d90d4f641060ce99caaf57c4e9e7132c3a87
mpalad.rails <happyfighter2015@gmail.com>
2019-09-05 20:02:03 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from collections import namedtuple
+from flask import Flask, render_template, request
+import praw
+
+app = Flask(__name__)
+
+Post = namedtuple('Post', ['title', 'selftext', 'url', 'score', 'num_comments'])
+
+
+def fetch_subreddit(subreddit_name):
+    reddit = praw.Reddit(client_id='opqOO7Uavtxmeg',
+                     client_secret='lXz6MRIK9AgagWlsKTTTxvCq-8o',
+                     user_agent='firstscript by /u/lakara20')
+    posts = []
+    for submission in reddit.subreddit(subreddit_name).hot(limit=15):
+        url = submission.url
+        if 'reddit.com' in url:
+            url = None
+        posts.append(Post(title=submission.title, selftext=submission.selftext, url=url,
+                          score=submission.score, num_comments=submission.num_comments))         
+    return posts
+        
+
+@app.route('/')
+def index():
+    posts = []
+    group_name = request.args.get('group')
+    if group_name:
+        posts = fetch_subreddit(group_name.strip())
+    return render_template('index.html', group_name=group_name, posts=posts)
+
