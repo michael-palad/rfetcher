6fcb4ebfe5c2057854bff51cf93beaad8ccc915b
mpalad.rails <happyfighter2015@gmail.com>
2019-09-08 13:39:46 -0400

    Add new or host submission option

--- app.py
+++ app.py
@@ -7,12 +7,18 @@ app = Flask(__name__)
 Post = namedtuple('Post', ['title', 'selftext', 'url', 'score', 'num_comments'])
 
 
-def fetch_subreddit(subreddit_name):
+def fetch_subreddit(subreddit_name, new_submissions=False):
     reddit = praw.Reddit(client_id='opqOO7Uavtxmeg',
                      client_secret='lXz6MRIK9AgagWlsKTTTxvCq-8o',
                      user_agent='firstscript by /u/lakara20')
     posts = []
-    for submission in reddit.subreddit(subreddit_name).hot(limit=15):
+    submissions = None
+    if new_submissions:
+        submissions = reddit.subreddit(subreddit_name).new(limit=15)
+    else:
+        submissions = reddit.subreddit(subreddit_name).hot(limit=15)
+
+    for submission in submissions:
         url = submission.url
         if 'reddit.com' in url or 'redd.it' in url:
             url = None
@@ -24,8 +30,9 @@ def fetch_subreddit(subreddit_name):
 @app.route('/')
 def index():
     posts = []
-    group_name = request.args.get('group')
+    group_name = request.args.get('group', '')
+    is_new = request.args.get('new', '0')
     if group_name:
-        posts = fetch_subreddit(group_name.strip())
-    return render_template('index.html', group_name=group_name, posts=posts)
+        posts = fetch_subreddit(group_name.strip(), (is_new == '1'))
+    return render_template('index.html', group_name=group_name, is_new=is_new, posts=posts)
 
